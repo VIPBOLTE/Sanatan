e30059f0331149df31c9dc3cb94f7e85dfefa154
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-02-28 20:40:22 +0530

    Update config.py

--- Sanatan/config.py
+++ Sanatan/config.py
@@ -1,24 +1,52 @@
-class Config(object):
-    LOGGER = True
-
-    # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
-
-    
-class Production(Config):
-    LOGGER = True
-
-
-class Development(Config):
-    LOGGER = True
+{
+    "name": "Sanatan",    
+    "env": {
+        "BOT_TOKEN": {
+            "description": "telegram bot token here",
+            "required": true
+        },        
+        "MONGO_URL": {
+            "description": "mongo db url here",
+            "required": true
+        },
+        "BOT_USERNAME": {
+            "description": "telegram bot username here without @",
+            "required": false
+        },
+        "OWNER_USERNAME": {
+            "description": "Owner Username without @",
+            "required": false
+        },
+        "BOT_NAME": {
+            "description": "Simple Bot-name. Do not use Modified Text..",
+            "required": false
+        },
+        "UPDATE_CHNL": {
+            "description": "Your Updates Channel without @",
+            "required": false
+        },
+        "SUPPORT_GRP": {
+            "description": "Your Support Group without @",
+            "required": false
+        },
+        
+        "START_IMG": {
+            "description": "Start Image For Bot.",
+            "required": false
+        },
+        
+        "API_ID": {
+            "description": "telegram api id here",
+            "required": true
+        },
+        "API_HASH": {
+            "description": "telegram api hash here",
+            "required": true
+        }
+    },
+    "addons": [
+        {
+            "plan": "heroku-postgresql"
+        }
+    ]
+}
