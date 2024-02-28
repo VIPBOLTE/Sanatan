7045b2fa27fc2a93e6f59743cee930b2d45e74fc
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-02-28 20:55:30 +0530

    Update trade.py

--- Sanatan/Module/trade.py
+++ Sanatan/Module/trade.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from shivu import user_collection, shivuu
+from Sanatan import user_collection, Sanatan
 
 pending_trades = {}
 
