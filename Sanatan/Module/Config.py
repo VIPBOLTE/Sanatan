ba02027ff43a1919e92232a238e948259a784865
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-02-29 15:13:03 +0530

    Update Config.py

--- Sanatan/Module/Config.py
+++ Sanatan/Module/Config.py
@@ -1,1 +1,52 @@
-
+{
+    "name": "Sanatan",    
+    "env": {
+        "BOT_TOKEN": {
+            "description": "telegram bot token here",
+            "required": "@bot_father"
+        },        
+        "MONGO_URL": {
+            "description": "mongo db url here",
+            "required": "mongodb.com"
+        },
+        "BOT_USERNAME": {
+            "description": "telegram bot username here without @",
+            "required": " "
+        },
+        "OWNER_USERNAME": {
+            "description": "Owner Username without @",
+            "required": " "
+        },
+        "BOT_NAME": {
+            "description": "Simple Bot-name. Do not use Modified Text..",
+            "required": " "
+        },
+        "UPDATE_CHNL": {
+            "description": "Your Updates Channel without @",
+            "required": " "
+        },
+        "SUPPORT_GRP": {
+            "description": "Your Support Group without @",
+            "required": " "
+        },
+        
+        "START_IMG": {
+            "description": "Start Image For Bot.",
+            "required": " "
+        },
+        
+        "API_ID": {
+            "description": "telegram api id here",
+            "required": " "
+        },
+        "API_HASH": {
+            "description": "telegram api hash here",
+            "required": " "
+        }
+    },
+    "addons": [
+        {
+            "plan": "heroku-postgresql"
+        }
+    ]
+}
