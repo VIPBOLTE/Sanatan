a8e310ffb2679474e8a80c487148d338932b1f31
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-01 11:33:00 +0530

    Update Config.py

--- Sanatan/Config.py
+++ Sanatan/Config.py
@@ -1,52 +1,29 @@
-{
-    "name": "Sanatan",    
-    "env": {
-        "BOT_TOKEN": {
-            "description": "telegram bot token here",
-            "required": "@bot_father"
-        },        
-        "MONGO_URL": {
-            "description": "mongo db url here",
-            "required": "mongodb.com"
-        },
-        "BOT_USERNAME": {
-            "description": "telegram bot username here without @",
-            "required": " "
-        },
-        "OWNER_USERNAME": {
-            "description": "Owner Username without @",
-            "required": " "
-        },
-        "BOT_NAME": {
-            "description": "Simple Bot-name. Do not use Modified Text..",
-            "required": " "
-        },
-        "UPDATE_CHNL": {
-            "description": "Your Updates Channel without @",
-            "required": " "
-        },
-        "SUPPORT_GRP": {
-            "description": "Your Support Group without @",
-            "required": " "
-        },
-        
-        "START_IMG": {
-            "description": "Start Image For Bot.",
-            "required": " "
-        },
-        
-        "API_ID": {
-            "description": "telegram api id here",
-            "required": " "
-        },
-        "API_HASH": {
-            "description": "telegram api hash here",
-            "required": " "
-        }
-    },
-    "addons": [
-        {
-            "plan": "heroku-postgresql"
-        }
-    ]
-}
+class Config(object):
+    LOGGER = True
+
+    # Get this value from my.telegram.org/apps
+    OWNER_ID = "5595153270"
+    sudo_users = "5595153270"
+    GROUP_ID = -1002126989582
+    TOKEN = "6902253047:AAGFi9KFbAaqZ6Qj6-fpTR2P5TxxV0omHRQ"
+    mongo_url = "mongodb+srv://babusona:hinatababy@cluster0.t0lfelh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
+    SUPPORT_CHAT = "https://t.me/goku_groupz"
+    UPDATE_CHAT = "https://t.me/channelz_k"
+    BOT_USERNAME = "@GOKU_CHATbot"
+    api_id = 13220924
+    api_hash = "5542f1adb4a900f648c985a6694fc3ed"
+    
+    STRICT_GBAN = True
+    ALLOW_CHATS = True
+    ALLOW_EXCL = True
+    DEL_CMDS = True
+    INFOPIC = True
+
+    
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
