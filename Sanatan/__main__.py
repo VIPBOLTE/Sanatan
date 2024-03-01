677a3da6ba135d5860edd132b53ad26a8f4b7d2c
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-01 22:32:04 +0530

    Update __main__.py

--- Sanatan/__main__.py
+++ Sanatan/__main__.py
@@ -25,7 +25,7 @@ message_counts = {}
 
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("Sanatan.module" + module_name)
+    imported_module = importlib.import_module("Sanatan.module." + module_name)
 
 
 last_user = {}
