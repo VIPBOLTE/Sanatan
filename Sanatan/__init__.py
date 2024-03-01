b147c5ce419260b6f8ebde7a92ecd8612ce18aba
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-01 19:23:43 +0530

    Update __init__.py

--- Sanatan/__init__.py
+++ Sanatan/__init__.py
@@ -15,7 +15,7 @@ logging.getLogger('httpx').setLevel(logging.WARNING)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-from Sanatan.Module.config import Development as Config
+from Sanatan.Module.Config import Development as Config
 
 
 api_id = Config.api_id
