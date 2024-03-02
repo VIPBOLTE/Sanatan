f22251ccab388ee73e320d3a46a9874fc42477f7
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-02 11:33:51 +0530

    Update upload.py

--- Sanatan/Module/upload.py
+++ Sanatan/Module/upload.py
@@ -132,7 +132,7 @@ async def update(update: Update, context: CallbackContext) -> None:
         if args[1] in ['name', 'anime']:
             new_value = args[2].replace('-', ' ').title()
         elif args[1] == 'rarity':
-            rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Special edition", 6 "🔮 Limited Edition", 7 "💎 Fantastic 😶"}
+            rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Special edition", 6: "🔮 Limited Edition", 7: "💎 Fantastic 😶"}
             try:
                 new_value = rarity_map[int(args[2])]
             except KeyError:
