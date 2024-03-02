1551148dba73a713d6aa01cc6edc9f65c002d35b
ishu9805 <abhishek47903@gmail.com>
2024-03-02 16:31:33 +0530

    Update upload.py

--- Sanatan/Module/upload.py
+++ Sanatan/Module/upload.py
@@ -13,7 +13,7 @@ img_url character-name anime-name rarity-number
 
 use rarity number accordingly rarity Map
 
-rarity_map = 1 (⚪️ Common), 2 (🟣 Rare) , 3 (🟡 Legendary), 4 (🟢 Medium), 5 (💮 Special edition), 6 (🔮 Limited Edition), 7 (💎 Fantastic 😶)"""
+rarity_map = 1 (⚪️ Common), 2 (🟣 Rare) , 3 (🟡 Legendary), 4 (🟢 Medium), 5 (💮 Special edition), 6 (🔮 Limited Edition), 7 (💎 Fantastic )"""
 
 
 
