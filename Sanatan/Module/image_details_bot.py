cbf321e613df95306488a76fdc640cf61a7440dc
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-02 18:04:36 +0530

    Update image_details_bot.py

--- Sanatan/Module/image_details_bot.py
+++ Sanatan/Module/image_details_bot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from Module import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 # Define your Telegram Bot API token here
 TOKEN = "your_telegram_bot_token_here"
