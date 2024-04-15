da292df786051ba66c41368999dc2da9f9bff7a6
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-15 23:46:00 +0530

    Update harem.py

--- Sanatan/Module/harem.py
+++ Sanatan/Module/harem.py
@@ -132,6 +132,6 @@ async def harem_callback(update: Update, context: CallbackContext) -> None:
 
 
 
-application.add_handler(CommandHandler(["harem", "collection"], harem,block=False))
+application.add_handler(CommandHandler(["harem", "seal", "collection"], harem,block=False))
 harem_handler = CallbackQueryHandler(harem_callback, pattern='^harem', block=False)
 application.add_handler(harem_handler)
