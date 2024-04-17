697040fd72521430ff41522800515a1b23c7d1e7
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-17 13:52:05 +0530

    Update Config.py

--- Sanatan/Config.py
+++ Sanatan/Config.py
@@ -4,12 +4,12 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "5595153270"
     sudo_users = "5595153270", "6092692622"
-    GROUP_ID = -1002126989582
-    TOKEN = "7167198617:AAGjuNQUX6FTzblOasSA5lQm4VutvvNkqyE"
+    LOGGER_ID= -1002126989582
+    BOT_TOKEN = "7167198617:AAGjuNQUX6FTzblOasSA5lQm4VutvvNkqyE"
     MONGO_URL = "mongodb+srv://babusona:hinatababy@cluster0.t0lfelh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     VIDEO_URL = ["https://telegra.ph/file/2f56b2568b3f92abcf55e.mp4"]
     SUPPORT_CHAT = "https://t.me/goku_groupz"
-    UPDATE_CHAT = "https://t.me/channelz_k"
+    SUPPORT_CHANNEL= "https://t.me/channelz_k"
     BOT_USERNAME = "@Husbandobot"
     API_ID = 13220924
     API_HASH = "5542f1adb4a900f648c985a6694fc3ed"
