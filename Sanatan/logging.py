c135d787c4ab74282e2e07891c4fb950edf318e4
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-07-16 12:07:05 +0530

    Update logging.py

--- Sanatan/logging.py
+++ Sanatan/logging.py
@@ -1,9 +1,5 @@
 class logging(object):
     LOGGER = True
-
-    # this bot sudo users
-    sudo_users = "5595153270", "6176582997"
-    
     
     STRICT_GBAN = True
     ALLOW_CHATS = True
@@ -11,10 +7,8 @@ class logging(object):
     DEL_CMDS = True
     INFOPIC = True
 
-    
 class Production(logging):
     LOGGER = True
-
 
 class Development(logging):
     LOGGER = True
