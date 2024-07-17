89a3118f16eb9014b24dce7d0b31b9dcffd911a3
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-07-17 15:49:24 +0530

    Update harem.py

--- Sanatan/Module/harem.py
+++ Sanatan/Module/harem.py
@@ -132,6 +132,6 @@ async def harem_callback(update: Update, context: CallbackContext) -> None:
 
 
 
-application.add_handler(CommandHandler(["husbendo", "collection"], harem,block=False))
+application.add_handler(CommandHandler(["anime", "collection"], harem,block=False))
 harem_handler = CallbackQueryHandler(harem_callback, pattern='^harem', block=False)
 application.add_handler(harem_handler)
